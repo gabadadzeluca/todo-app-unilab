5d152da937c09a99435ac1e39c1ce630a8e55bd1
Luca <gabadadzeluca@gmail.com>
2023-04-27 00:05:30 +0400

    add startingScreen component and redirect logic

--- src/components/startingScreen/StartingScreen.tsx
+++ src/components/startingScreen/StartingScreen.tsx
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function StartingScreen(){
+  const navigate = useNavigate();
+
+  const handleGetStartedClick = () => {
+    navigate('/login');
+  };
+
   return (
-    <>HELLO</>
+    <div>
+      <img src='' alt=''/>
+      <h2>Keep track of daily tasks in life</h2>
+      <button onClick={handleGetStartedClick}>Get Started</button>
+    </div>
   );
 }
