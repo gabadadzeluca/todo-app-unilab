43de3653c5bb81219fd2d2938ad7f057c4c520c7
Luca <gabadadzeluca@gmail.com>
2023-04-26 23:20:50 +0400

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Navigate } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
