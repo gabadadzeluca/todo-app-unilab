412c4a92fbb235f0d43e535b503c0ff1e0292c9b
Luca <gabadadzeluca@gmail.com>
2023-04-26 23:45:41 +0400

    add basic router logic, test-only

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import './App.css'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(localStorage.getItem('isLoggedIn') === 'true');
+  console.log("IS LOGGED IN:", isLoggedIn);
+
+  const handleLogin = (isLoggedIn:boolean):void =>{
+    setIsLoggedIn(isLoggedIn);
+  }
 
   return (
-    <>
-    </>
+   <BrowserRouter>
+    <Routes>
+        <Route path="/" element={isLoggedIn ? <Navigate to="/main" /> : <div>NOT LOGGED IN</div>} />
+        <Route path="/sign_in" element={isLoggedIn ? <div>TEST</div> : <Navigate to="/" />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
