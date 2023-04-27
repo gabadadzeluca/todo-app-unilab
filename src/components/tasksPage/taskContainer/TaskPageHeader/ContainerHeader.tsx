af704c4f80919101bf6135edebf98127183c53ef
Luca <gabadadzeluca@gmail.com>
2023-04-28 00:37:36 +0400

    add props to container header, display user's parameters

--- src/components/tasksPage/taskContainer/TaskPageHeader/ContainerHeader.tsx
+++ src/components/tasksPage/taskContainer/TaskPageHeader/ContainerHeader.tsx
@@ -1,10 +1,12 @@
-export default function ContainerHeader(){
+import UserType from "../../../../utils/UserType";
+export default function ContainerHeader(props: UserType){
+  const { name, imageUrl } = props;
   return (
     <div>
       <h3>to do</h3>
       <div>
-        <p>name</p>
-        <img src='' alt='avatar' />
+        <p>{name ?? 'name'}</p>
+        <img src={imageUrl} alt='avatar' />
       </div>
     </div>
   );
