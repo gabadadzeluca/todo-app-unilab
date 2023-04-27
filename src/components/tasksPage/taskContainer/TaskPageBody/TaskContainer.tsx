f04c2af0a49b5fc5993a193bd3d37f49bec85b7d
Luca <gabadadzeluca@gmail.com>
2023-04-28 00:38:56 +0400

    pass props to ContainerHeader and add types

--- src/components/tasksPage/taskContainer/TaskPageBody/TaskContainer.tsx
+++ src/components/tasksPage/taskContainer/TaskPageBody/TaskContainer.tsx
@@ -4,10 +4,12 @@ import UserType from "../../../../utils/UserType";
 export default function TaskContainer(){
   const userString: string|null = localStorage.getItem('user');
   const user: UserType|null = userString ? JSON.parse(userString) : null;
-  console.log("USER:", user);
+  const name:string|undefined = user?.name;
+  const imageUrl:string|undefined = user?.imageUrl;
+
   return (
     <div>
-      <ContainerHeader />
+      <ContainerHeader name={name} imageUrl={imageUrl}/>
     </div>
   );
 }
