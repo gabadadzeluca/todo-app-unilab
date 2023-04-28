2d5483a91d16b4c65752a109353697572edcbb5f
Luca <gabadadzeluca@gmail.com>
2023-04-28 20:16:30 +0400

    import taskReducer and pass the props to taskInput

--- src/components/tasksPage/TaskPageBody/taskContainer/TaskContainer.tsx
+++ src/components/tasksPage/TaskPageBody/taskContainer/TaskContainer.tsx
@@ -1,7 +1,9 @@
+import { useReducer } from 'react';
 import ContainerHeader from "../../TaskPageHeader/ContainerHeader";
 import UserType from "../../../../utils/UserType";
 import TaskInterface from "../../../../utils/TaskInterface";
 import TaskInput from "../taskInput/TaskInput";
+import { taskReducer, ACTIONS } from "../TaskReducer";
 
 export default function TaskContainer(){
   const userString: string|null = localStorage.getItem('user');
@@ -12,18 +14,20 @@ export default function TaskContainer(){
   // check for tasks array, if there is not any=> create array
   if(! localStorage.getItem('tasks')) localStorage.setItem('tasks', JSON.stringify([]));
   const taskString:string|null = localStorage.getItem("tasks");
-  let tasks:TaskInterface[] = [];
+  let tasksArray:TaskInterface[] = [];
 
   if(taskString){
-    tasks = JSON.parse(taskString);
+    tasksArray = JSON.parse(taskString);
   }
+  const [tasks, dispatch] = useReducer(taskReducer, tasksArray);
 
-  console.log("TASKS:", tasks);
   return (
     <div>
       <ContainerHeader name={name} imageUrl={imageUrl}/>
-      <TaskInput tasks={tasks}/>
-      {/* map tasks=> */}
+      <TaskInput dispatch={dispatch} tasks={tasks}/>
+      {tasks.map(task=>{
+        return <p>{task.body}</p>
+      })}
     </div>
   );
 }
