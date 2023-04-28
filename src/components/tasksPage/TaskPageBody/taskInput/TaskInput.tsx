0cfa16212c4ce1b6d9c0a0a780698a4e9ce894e5
Luca <gabadadzeluca@gmail.com>
2023-04-28 18:48:26 +0400

    add reducer function and additional types

--- src/components/tasksPage/TaskPageBody/taskInput/TaskInput.tsx
+++ src/components/tasksPage/TaskPageBody/taskInput/TaskInput.tsx
@@ -1,6 +1,34 @@
 import { useRef, useReducer } from 'react';
 import TaskInterface from '../../../../utils/TaskInterface';
 
+
+const ACTIONS = {
+  'ADD_TASK': 'ADD_TASK',
+  'REMOVE_TASK': 'REMOVE_TASK',
+}
+
+interface AddTaskAction{
+  type: 'ADD_TASK';
+  payload: TaskInterface;
+}
+interface RemovTaskAction{
+  type: 'REMOVE_TASK';
+  payload: TaskInterface;
+}
+type ActionType = AddTaskAction | RemovTaskAction
+
+const taskReducer = (state:TaskInterface[], action:ActionType) => {
+  switch(action.type){
+    case ACTIONS.ADD_TASK:
+     return [...state, action.payload];
+    case ACTIONS.REMOVE_TASK:
+      const filteredTasks = state.filter((task) => task.created_at !== action.payload.created_at);
+      return filteredTasks;
+    default : 
+      throw new Error('Unrecognised action');
+  }
+}
+
 export default function TaskInput(props:{
   tasks: TaskInterface[];
 }){
