3fa5a3797c843789dab577f281c414af4c0281b7
Luca <gabadadzeluca@gmail.com>
2023-04-28 21:08:52 +0400

    add basic task component strucure & functions to delete or mark tasks complete

--- src/components/tasksPage/task/TaskComponent.tsx
+++ src/components/tasksPage/task/TaskComponent.tsx
@@ -1,5 +1,29 @@
-export default function TaskComponent(){
+import TaskInterface from "../../../utils/TaskInterface";
+import { ACTIONS, ActionType } from "../TaskPageBody/TaskReducer";
+
+interface TaskPropsInterface{
+  task: TaskInterface;
+  dispatch: (action: ActionType) => void;
+}
+export default function TaskComponent(props:TaskPropsInterface){
+  const {task, dispatch} = props;
+  const deleteTask = (task:TaskInterface):void => {
+    dispatch({type: ACTIONS.REMOVE_TASK, payload:task});
+  }
+  const markCompleted = (task:TaskInterface):void => {
+    dispatch({type: ACTIONS.MARK_COMPLETED, payload: task})
+  }
+  
   return (
-    <></>
+    <div>
+      <p>
+        {task.body}
+      </p>
+      <div>
+        {/* div for the buttons */}
+        <button>mark completed</button>
+        <button>delete</button>
+      </div>
+    </div>
   );
 }
