5537a09df952745201c665ead1949709f13ab76d
Luca <gabadadzeluca@gmail.com>
2023-05-01 00:50:23 +0400

    add classnames from the module

--- src/components/startingScreen/StartingScreen.tsx
+++ src/components/startingScreen/StartingScreen.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import todoIcon from "../../assets/todo-icon.png"
+import styles from "./StartingScreen.module.css";
+
 
 export default function StartingScreen(){
   const navigate = useNavigate();
@@ -8,10 +11,12 @@ export default function StartingScreen(){
   };
 
   return (
-    <div>
-      <img src='' alt=''/>
-      <h2>Keep track of daily tasks in life</h2>
-      <button onClick={handleGetStartedClick}>Get Started</button>
+    <div className={styles.startingScreenContainer}>
+      <div className={styles.startingScreenDiv}>
+        <img src={todoIcon} alt=''/>
+        <h2>Keep track of daily tasks in life</h2>
+        <button onClick={handleGetStartedClick}>Get Started</button>
+      </div>
     </div>
   );
 }
